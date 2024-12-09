eb4325711c19f510aa68bd9e0041203aa5418274
LucasFigueroa <lucasafigueroa93@gmail.com>
2024-12-09 20:59:06 -0300

    [M] Variables de entorno

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD-c1_06-_GS4ovqTimfPdD4mW4dC9p8m4",
-    authDomain: "backend-my-app-store.firebaseapp.com",
-    projectId: "backend-my-app-store",
-    storageBucket: "backend-my-app-store.firebasestorage.app",
-    messagingSenderId: "610265546176",
-    appId: "1:610265546176:web:09b11f2a4a32ef21451c25"
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGE,
+    messagingSenderId: import.meta.env.VITE_MESSAGING,
+    appId: import.meta.env.VITE_APPID
 };
 
 const app = initializeApp(firebaseConfig);
